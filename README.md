35a6191a92f3b7c2d5c668f2a2c24dbbd7f802d0
Helen <helen_kattaleeya@outlook.com>
2023-04-14 19:29:26 +0900

    inital set up and pseudocode

---
+++ README.md
@@ -0,0 +1,6 @@
+# Summary
+
+This project deals with Dog Breed Classification using prelabelled "Standford Dogs" data. Tasks showcased in this project includes:
+- data preprocessing
+- building and training a model using Tensorflow
+- building a simple classification script to run the trained model
---
+++ classification-model.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+# read in dataset
+# -- check dataset images by displaying random image
+images = []
+annotations = []
+
+
+# split dataset into train/test/val dataset
+# -- dataset processing
+
+# build classification model
+
+# train model
+
+# test model on validation set
